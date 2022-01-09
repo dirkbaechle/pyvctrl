66713a06e3a71206e3a2da30269cb2652c301d02
Dirk Baechle <dl9obn@darc.de>
2022-01-09 13:10:40 +0100

    Corrected handling of signed chars in filter.

--- scripts/filter_vclient_log.py
+++ scripts/filter_vclient_log.py
@@ -4,6 +4,8 @@ import sys
 FMLOG = 0
 # Filter for only listing the valid addresses
 FMADDRESS = 1
+# Filter for listing key and value, with the latter in hex
+FMHEX = 2
 
 # Parser in void mode
 PMVOID = 0
@@ -21,8 +23,28 @@ def outputAddress(key, value):
     key = key.rstrip(':')
     print(key)
 
+def outputHex(key, value):
+    print(key)
+    try:
+        # Split off remaining floating-point parts
+        if '.' in value:
+            pidx = value.find('.')
+            value = value[:pidx]
+        elif ',' in value:
+            pidx = value.find(',')
+            value = value[:pidx]
+        if value.startswith('-'):
+            # Convert to unsigned char
+            hval = "%02X" % (int(value)+2**8)
+        else:
+            hval = "%02X" % int(value)
+    except:
+        hval = "??"
+    print(hval)
+
 output = {FMLOG : outputLog,
-          FMADDRESS : outputAddress}
+          FMADDRESS : outputAddress,
+          FMHEX : outputHex}
 
 def parseFile(fpath, outmode):
     pmode = PMVOID
@@ -30,6 +52,7 @@ def parseFile(fpath, outmode):
     with open(fpath, "r") as fin:
         for line in fin.readlines():
             line = line.rstrip('\n')
+            line = line.rstrip()
             if 'SRV ERR:' in line or 'rror' in line:
                 pmode = PMERROR
             else:
@@ -44,13 +67,18 @@ def parseFile(fpath, outmode):
                 elif pmode == PMKEY:
                     if line:
                         # Accept next line as value if it's not empty
+                        if '.' in line or ',' in line:
+                            # Also reduce trailing zeros
+                            while line.endswith("00"):
+                                line = line[:-1]
+
                         output[outmode](ckey, line)
                         pmode = PMVOID
 
 
 def usage():
     print("filter_vclient_log v1.0, Dirk Baechle, 2022-01-09")
-    print("Usage: filter_vclient_log [-l] [-a] <logfile>")
+    print("Usage: filter_vclient_log [-l] [-a] [-h] <logfile>")
 
 def main():
     outmode = FMLOG
@@ -61,6 +89,8 @@ def main():
             outmode = FMLOG
         elif arg in ['-a', '--address', '--address-mode']:
             outmode = FMADDRESS
+        elif arg in ['-h', '--hexlog', '--hexlog-mode']:
+            outmode = FMHEX
         else:
             fpath = arg
 
